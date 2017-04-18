c34b6f1fbb310b6a91d803d093a35a893ccf2dc1
mike stedman <github@ravuya.com>
2017-04-18 14:58:13 -0600

    Reproduce this strange crash bug when sharing a Series

---
+++ ScrollChart/DemoViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Linq;
+using System.Windows.Threading;
+
+namespace ScrollChart
+{
+    class DemoViewModel : ViewModelBase
+    {
+        public SeriesCollection Series { get; private set; }
+
+        public DemoViewModel()
+        {
+            Series = new SeriesCollection()
+            {
+                new LineSeries() { Title = "Age", Values = new ChartValues<double>() }
+            };
+
+            var timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var lineSeries = Series.First();
+            var values = lineSeries.Values as ChartValues<double>;
+            values.Add(Math.Cos(Environment.TickCount / 1000.0));
+        }
+    }
+}
